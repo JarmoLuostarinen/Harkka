041618e5c3ff7c168d8316f12b1e47ad2af543ab
Jarmo Luostarinen <K3295@student.jamk.fi>
2016-01-19 11:45:09 +0200

    Kotityö

---
+++ Demo03/Kotitehtävä4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtävä4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one car instance
+
+            Vehicle vehicle = new Vehicle();
+
+            vehicle.Name = "Audi";
+            vehicle.Speed = 180;
+            vehicle.Tyres = 205;
+            vehicle.PrintData();
+            Console.WriteLine(vehicle.ToString());
+
+            Console.ReadLine();
+          
+           
+
+          
+        }
+    }
+}
---
+++ Demo03/Kotitehtävä4/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtävä4
+{
+    class Vehicle
+    {
+        // properties
+
+        public string Name { get; set; }
+
+        public int Tyres { get; set; }
+
+        public int Speed { get; set; }
+
+        //method display Vehicle data
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle data:");
+            Console.WriteLine("- Name : " + Name);
+            Console.WriteLine("- tyres : " + Tyres);
+            Console.WriteLine("- Speed :" + Speed);
+
+        }
+        public override string ToString()
+        {
+            return "Name " + Name + "tyres" + Tyres + "Speed" + Speed;
+        }
+    }
+}
