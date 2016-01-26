bc4bd90c98b6e8ae2296e80dfdc7129b275a754d
Jarmo Luostarinen <K3295@student.jamk.fi>
2016-01-26 10:50:37 +0200

    new

--- Kulkuneuvo/Kulkuneuvo/Program.cs
+++ Kulkuneuvo/Kulkuneuvo/Program.cs
@@ -11,16 +11,18 @@ namespace Kulkuneuvo
         static void Main(string[] args)
         {
             // create tyre
-            Tyre tyre = new Tyre { Name = "Nokia", Type = "Nasta", Size = "185R14" };
-        
+            Tyre tyre1 = new Tyre { Name = "Nokia", Type = "Nasta", Size = "185R14" };
+            Tyre tyre2 = new Tyre { Name = "GoodYear", Type = "Summer", Size = "225R17" };
+            Tyre tyre3 = new Tyre { Name = "YokoHama", Type = "Kitka", Size = "185R13" };
 
-        
+
+
             Vehicle car = new Vehicle { Name = "Datsun", Engine = " Nissan", Model = "100A" };
 
-            car.AddTyre(tyre);
-            car.AddTyre(tyre);
-            car.AddTyre(tyre);
-            car.AddTyre(tyre);
+            car.AddTyre(tyre1);
+            car.AddTyre(tyre1);
+            car.AddTyre(tyre3);
+            car.AddTyre(tyre3);
 
             Console.WriteLine(car.ToString());
             Console.WriteLine("Press erter");
