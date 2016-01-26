ceb662c89a2d9327ed0b77887ab83eca75275190
Jarmo Luostarinen <K3295@student.jamk.fi>
2016-01-26 10:47:03 +0200

    auto

---
+++ Kulkuneuvo/Kulkuneuvo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulkuneuvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create tyre
+            Tyre tyre = new Tyre { Name = "Nokia", Type = "Nasta", Size = "185R14" };
+        
+
+        
+            Vehicle car = new Vehicle { Name = "Datsun", Engine = " Nissan", Model = "100A" };
+
+            car.AddTyre(tyre);
+            car.AddTyre(tyre);
+            car.AddTyre(tyre);
+            car.AddTyre(tyre);
+
+            Console.WriteLine(car.ToString());
+            Console.WriteLine("Press erter");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Kulkuneuvo/Kulkuneuvo/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulkuneuvo
+{
+    class Vehicle : Tyre
+    {
+        
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Engine { get; set; }
+        public List<Tyre> Tyres { get; }
+
+        public Vehicle()
+        {
+            Tyres = new List<Tyre>();
+        }
+        public void AddTyre(Tyre tyre)
+        {
+            Tyres.Add(tyre);
+        }
+        public override string ToString()
+        {
+            string sVehicle = "Vehicle Name: " + Name + " Model: " + Model + " Tyres: \n";
+            foreach(Tyre tyre in Tyres)
+            {
+                sVehicle += tyre.ToString() + "\n";
+
+            }
+            return sVehicle;
+        }
+
+
+    }
+}
