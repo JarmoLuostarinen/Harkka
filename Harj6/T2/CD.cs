1af135b48448b98162a445e6c6a84c9faf916a80
Jarmo Luostarinen <K3295@student.jamk.fi>
2016-01-27 15:05:03 +0200

    6.T2

---
+++ Harj6/T2/CD.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class CD
+    {
+        /// <summary>
+        /// CD collection stores song objects
+        /// </summary>
+        public List<Song> cD;
+
+        /// <summary>
+        /// Default constructor initializes a collection
+        /// </summary>
+        public CD()
+        {
+            cD = new List<CD>();
+        }
+
+        /// <summary>
+        /// Adds a song to collection
+        /// </summary>
+        /// <param name="song">Added song</param>
+        public void AddPerson(CD cD)
+        {
+            cD.Add(cD);
+        }
+        /// <summary>
+        /// Returns a song to caller application
+        /// </summary>
+        /// <param name="index">song index in collection</param>
+        /// <returns>Returned song object</returns>
+        public song GetSong(int index)
+        {
+            if (index < song.Count) return cD.ElementAt(index);
+            else return null;
+        }
+
+        public song FindPerson(string sosialSecurityNumber)
+        {
+            foreach (Person person in persons)
+            {
+                if (sosialSecurityNumber.Equals(person.SosialSecurityNumber))
+                {
+                    return person;
+                }
+            }
+            // person with sotu not found...
+            return null;
+        }
+
+        public void PrintCollection()
+        {
+            foreach
+                  (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
---
+++ Harj6/T2/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Song
+    {
+        public string Album { get; set; }
+        public string SongName { get; set; }
+        public string Time { get; set; }
+
+        public override string ToString()
+        {
+            return Firstname + " " + Lastname + " " + SosialSecurityNumber;
+        }
+    }
+}
