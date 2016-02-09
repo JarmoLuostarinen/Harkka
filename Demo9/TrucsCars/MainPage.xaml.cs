7660381ba6247b6d548e4fd43738b15089c068a2
Jarmo Luostarinen <K3295@student.jamk.fi>
2016-02-09 11:10:30 +0200

    Ei toimi kuyitenkaan!!

---
+++ Demo9/TrucsCars/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TrucsCars
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+      
+
+        private void Trucks_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int truck = int.Parse(TruckBlock.Text);
+            truck++;
+            TruckBlock.Text = truck.ToString();
+        }
+
+        private void Cars_Click(object sender, RoutedEventArgs e)
+        {
+           
+            int car = int.Parse(CarBlock.Text);
+            car++;
+            CarBlock.Text = car.ToString();
+        }
+
+    }
+}
